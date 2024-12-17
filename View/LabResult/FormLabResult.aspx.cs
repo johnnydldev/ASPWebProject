b8e643645e8723fbd8b3eff85ad221e1feead885
johnnydldev <johnnydldev@gmail.com>
2024-12-17 12:21:21 -0600

    Upload frontend of lab result model

--- View/LabResult/FormLabResult.aspx.cs
+++ View/LabResult/FormLabResult.aspx.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using AspWebProject.Tools;
+using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VO;
 
 namespace AspWebProject.View.LabResult
 {
@@ -11,7 +15,113 @@ namespace AspWebProject.View.LabResult
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Validate of this is Postback
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["Id"] == null)
+                {
+                    //Header of form
+                    title.Text = "Agregar resultado";
+                    subTitle.Text = "Registro de una nuevo resultado";
+                }
+                else
+                {
+                    //To update
+                    //Got the id arrived of URL
+                    int _id = Convert.ToInt32(Request.QueryString["Id"]);
 
+                    //Gettting the info according to the id specified
+                    VO_Laboratory_Result labResult = BLLLaboratoryResult.GetLaboratoryResultById(_id);
+
+                    Console.WriteLine("El id es: " + _id);
+
+                    //Validate that exist the record in the table, if isn't that return to form
+                    if (labResult.IdLaboratoryResult != 0)
+                    {
+                        //Assign the new values to record according to the id labResult selected
+                        title.Text = "Actualizar resultado";
+                        subTitle.Text = $"Modificar los datos del resultado #{_id}";
+                        txtTest.Text = labResult.Test;
+                        txtResult.Text = labResult.ResultValue;
+                        txtDateDone.Text = labResult.DateDone;
+
+                        Console.WriteLine("El objeto es: " + labResult);
+                    }
+                    else
+                    {
+                        //Return to index page of labResult
+                        Response.Redirect("IndexLabResult.aspx");
+                    }
+                }
+
+            }
+
+        }//End load event
+
+        protected void btnSaveClick(object sender, EventArgs e)
+        {
+            string title = "", response = "", type = "", outputResult = "";
+            try
+            {
+                //Create the instance of new object labResult
+                VO_Laboratory_Result labResult = new VO_Laboratory_Result();
+                labResult.Test = txtTest.Text;
+                labResult.ResultValue = txtResult.Text;
+                labResult.DateDone = txtDateDone.Text;
+
+                //Verify if the info is to create or update
+                if (Request.QueryString["Id"] == null)
+                {
+                    outputResult = BLLLaboratoryResult.CreateLabResult(labResult);
+
+                    if (!outputResult.ToUpper().Contains("ERROR"))
+                    {
+                        title = "Ops...";
+                        response = outputResult;
+                        type = "warning";
+
+                    }
+
+                    HttpContext.Current.Response.Redirect("IndexlabResult.aspx");
+
+                }
+                else
+                {
+                    //Update
+                    labResult.IdLaboratoryResult = int.Parse(Request.QueryString["Id"]);
+                    outputResult = BLLLaboratoryResult.UpdateLabResult(labResult);
+                    HttpContext.Current.Response.Redirect("IndexLabResult.aspx");
+                }
+
+                //Preparing the info to show the errors through the Seet Alert
+                if (outputResult.ToUpper().Contains("ERROR"))
+                {
+                    title = "Ops...";
+                    response = outputResult;
+                    type = "warning";
+                }
+                else
+                {
+                    title = "Correcto!";
+                    response = outputResult;
+                    type = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                title = "Error";
+                response = ex.Message;
+                type = "error";
+            }
+            //sweet alert
+            SweetAlert.Sweet_Alert(title, response, type, this.Page, this.GetType(), "/view/labResult/IndexlabResult.aspx");
+
+        }//End save event
+
+        protected void btnBackClick(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Redirect("IndexLabResult.aspx");
         }
-    }
-}
+
+    }//End lab result class
+}//End namespace
--- View/LabResult/FormLabResult.aspx.designer.cs
+++ View/LabResult/FormLabResult.aspx.designer.cs
@@ -13,5 +13,95 @@ namespace AspWebProject.View.LabResult
 
     public partial class FormLabResult
     {
+
+        /// <summary>
+        /// Control title.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label title;
+
+        /// <summary>
+        /// Control subTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subTitle;
+
+        /// <summary>
+        /// Control lblTest.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTest;
+
+        /// <summary>
+        /// Control txtTest.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTest;
+
+        /// <summary>
+        /// Control lblResult.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResult;
+
+        /// <summary>
+        /// Control txtResult.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtResult;
+
+        /// <summary>
+        /// Control lblDateDone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDateDone;
+
+        /// <summary>
+        /// Control txtDateDone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDateDone;
+
+        /// <summary>
+        /// Control btnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSave;
+
+        /// <summary>
+        /// Control btnBack.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBack;
     }
 }
